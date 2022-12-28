48fe5a26f05b8c444c70ac07bf48a3bb0dd48332
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2022-12-28 21:44:56 +0100

    Guardar todo en el repertorio correcto

--- tilesets/republique.tsx
+++ tilesets/republique.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="republique" tilewidth="32" tileheight="32" tilecount="10440" columns="120">
- <image source="../republique.png" width="3850" height="2807"/>
+<tileset version="1.9" tiledversion="1.9.1" name="republique" tilewidth="32" tileheight="32" tilecount="10440" columns="120">
+ <image source="republique.png" width="3850" height="2807"/>
 </tileset>
