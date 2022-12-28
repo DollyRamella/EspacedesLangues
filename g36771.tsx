cda2d1a466e447a03db54a273d5af8522dda90ea
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2022-12-28 21:31:22 +0100

    Hice un bonito mapa

---
+++ g36771.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="g36771" tilewidth="32" tileheight="32" tilecount="10440" columns="120">
+ <image source="g36771.png" width="3850" height="2807"/>
+</tileset>
---
+++ tilesets/roads.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="roads" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="roads.png" width="256" height="320"/>
+ <tile id="68">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
