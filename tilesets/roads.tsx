816b31567d02661d1943654177a4b46fde06c0e9
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2023-01-10 22:25:06 +0100

    Test d'animation de tuile

--- tilesets/roads.tsx
+++ tilesets/roads.tsx
@@ -1,9 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="roads" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+<tileset version="1.9" tiledversion="1.9.1" name="roads" tilewidth="32" tileheight="32" tilecount="80" columns="8">
  <image source="roads.png" width="256" height="320"/>
  <tile id="68">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
+ </tile>
+ <tile id="70">
+  <animation>
+   <frame tileid="42" duration="300"/>
+   <frame tileid="49" duration="300"/>
+   <frame tileid="41" duration="300"/>
+   <frame tileid="50" duration="300"/>
+  </animation>
  </tile>
 </tileset>
