c62df4154ca5dbf4142fb7a94428a923f812030b
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2023-01-10 22:51:09 +0100

    Plus d'animations

---
+++ tilesets/bubbles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="bubbles" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="Orbz/OrbzPrw.png" width="330" height="132"/>
+</tileset>
---
+++ tilesets/helena.tsx
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="helena" tilewidth="20" tileheight="20" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="20" height="20" source="helena/cracks1.png"/>
+  <animation>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="15" duration="300"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="15" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="12" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="20" height="20" source="helena/cracks2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="20" height="20" source="helena/cracks3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="20" height="20" source="helena/cracks4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="20" height="20" source="helena/cracks5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="20" height="20" source="helena/cracks6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="20" height="20" source="helena/cracks7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="20" source="helena/cracks8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="20" height="20" source="helena/cracks9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="20" source="helena/cracks10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="20" height="20" source="helena/cracks11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="20" height="20" source="helena/enemyselected.png"/>
+ </tile>
+ <tile id="12">
+  <image width="20" height="20" source="helena/expl1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="20" height="20" source="helena/expl2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="20" height="20" source="helena/expl3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="20" height="20" source="helena/expl4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="20" height="20" source="helena/expl5.png"/>
+ </tile>
+ <tile id="17">
+  <image width="20" height="20" source="helena/expl6.png"/>
+ </tile>
+ <tile id="18">
+  <image width="20" height="20" source="helena/expl7.png"/>
+ </tile>
+ <tile id="19">
+  <image width="20" height="20" source="helena/expl8.png"/>
+ </tile>
+ <tile id="20">
+  <image width="20" height="20" source="helena/expl9.png"/>
+ </tile>
+ <tile id="21">
+  <image width="20" height="20" source="helena/expl10.png"/>
+ </tile>
+ <tile id="22">
+  <image width="20" height="20" source="helena/H0.png"/>
+ </tile>
+ <tile id="23">
+  <image width="20" height="20" source="helena/H1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="20" height="20" source="helena/H2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="20" height="20" source="helena/H3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="20" height="20" source="helena/H4.png"/>
+ </tile>
+ <tile id="27">
+  <image width="20" height="20" source="helena/H5.png"/>
+ </tile>
+ <tile id="28">
+  <image width="20" height="20" source="helena/H6.png"/>
+ </tile>
+ <tile id="29">
+  <image width="20" height="20" source="helena/H7.png"/>
+ </tile>
+</tileset>
