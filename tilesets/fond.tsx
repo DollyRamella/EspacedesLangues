cf5faeb2eb51ecb105566e39737f1eedac290547
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2023-03-05 14:54:22 +0100

    MAJ2 Jitsi

---
+++ tilesets/fond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="fond " tilewidth="32" tileheight="32" tilecount="133560" columns="360">
+ <image source="Villejean.png" width="11535" height="11879"/>
+</tileset>
---
+++ tilesets/fond2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Fond2" tilewidth="32" tileheight="32" tilecount="7300" columns="33">
+ <image source="Villejean2.png" width="1069" height="779"/>
+</tileset>
